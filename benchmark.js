da3c306af50657d6c334094835e666980280f0dd
esabox <esabox@live.jp>
2019-12-04 23:11:50 +0900

    ベンチを直しまくり

--- benchmark.js
+++ benchmark.js
@@ -9,20 +9,20 @@ const log = console['log']
 //二重ループにして平均取る、二重目が100未満だと誤差が激しすぎる。
 class Bench {
 
-	constructor(loop1, loop2 = 100) {
+	constructor(loop1 = 10, loop2 = 1000) {
 		this.loop2 = loop2
-		this.lll1 = loop1
+		this.loop1 = loop1
 		this.head = 'b.add('
-		this.times = []
+		this.obj_arr = [{func: () => new Object(), time: 0, }]
 		this.performance = (typeof exports === 'object')
 			? require('perf_hooks').performance
 			: window.performance
 
 	}
 
-	lp(func) {
+	do_func_loop_and_returnTime(func, loop) {
 		let time = performance.now() //時間測定this.this.
-		for (let i = 0, l = this.loop2; i < l; i++) func()
+		for (let i = 0; i < loop; i++) func()
 		time = performance.now() - time
 
 		//this.logs.push(func)
@@ -30,69 +30,185 @@ class Bench {
 		//this.times.push(time)
 	}
 	add(func) {
-		if (!this.funcs) this.funcs = []
-		this.funcs.push(func)
-		this.times.push(0)
+		this.obj_arr.push({func: func, time: 0, result: ''})
 	}
 	play() {
-		//時間配列を0で初期化
-		// this.times=Array(this.funcs.length)
-		// this.times.fill(0)
+
+		//結果だけとりあえず回収
+		for (let val of this.obj_arr) {
+			val.result = val.func()
+			val.type = typeof val.result
+		}
 
 		//ループ一次元
-		for (let j = 0; j < this.lll1; j++) {
-			let i = 0
-			for (let [key, val] of Object.entries(this.funcs)) {
-				this.times[key] += this.lp(val)
-				i++
+		for (let j = 0; j < this.loop1; j++) {
+			for (let [key, val] of Object.entries(this.obj_arr)) {
+				val.time += this.do_func_loop_and_returnTime(val.func, this.loop2)
+				// this.times[key] = val.time
 			}
 			//this.prt()
 		}
+
+		return this.obj_arr.shift() && this.obj_arr //.unshift(1)
 	}
+
+	_maxleng(arr) {
+		const len_arr = arr.map(val => ('' + val).length)
+		return Math.max(...len_arr)
+	}
+
+	_padding2(arrObj, key, newKey = key, align = 'right') {
+		//配列としてコピーを作る
+		let arr = arrObj.map(o => o[key])
+		// arr = this._padding(arr)
+		// let maxlen = this._maxleng(arr)
+		String.prototype.zen2_lenght = function() {
+			return this.replace(/[^\x01-\x7E]/g, '**').length
+		}
+		const maxlen = (() => {
+			//const dec=arr.map(val)
+			const dec = arr.map(val => ('' + val).replace(/[^\x01-\x7E]/g, '**'))
+			log(dec)
+			const len_arr = arr.map(val => ('' + val).zen2_lenght())
+			// const len_arr = dec.map(val => ('' + val).length)
+			// const len_arr = arr.map(val => ('' + val).length)
+			log(len_arr)
+			return Math.max(...len_arr)
+		})()
+		//パディングで空白足して切り取ってたが、必要な空白を足したほうが良さそう。
+		function sliceZenkaku(str, arg) {
+			const zenkakuCount = (str.match(/[^\x01-\x7E]/g) || []).length
+			const len = str.length
+			log(str, zenkakuCount)
+			arg.length === 1
+				? arg[0] += zenkakuCount
+				: arg[1] -= zenkakuCount
+			return str.slice(...arg)
+		}
+		//空白を足すタイプに書き換え
+		function padd(val, maxlen, opt = {align: 'left'}) {
+			let str=''+val
+			const len = str.zen2_lenght()
+			const space = ' '.repeat(maxlen - len)
+			if (opt.align === 'left') str = str + space
+			if (opt.align === 'right') str = space + str
+			return str
+		}
+		arr = arr.map(val => padd(val, maxlen, {align: align}))
+			// (align === 'right')
+			// ? arr.map(val => padd('' + val, maxlen, {align: align}))
+			// : arr.map(val => padd('' + val, maxlen, {align: 'left'}))
+			// ? arr.map(val => sliceZenkaku(' '.repeat(maxlen) + val, [-maxlen]))
+			// : arr.map(val => sliceZenkaku(val + ' '.repeat(maxlen), [0, maxlen]))
+		// ? arr.map(val => (' '.repeat(maxlen) + val).slice(-maxlen))
+		// : arr.map(val => (val + ' '.repeat(maxlen)).slice(0, maxlen))
+
+		//戻す
+		arrObj.map((o, i) => o[newKey] = arr[i])
+	}
+
 	print_result() {
+		let objArr = this.play()
+
 		//ヘッダー
 		let str = '\n'
-		let [l1, l2] = [this.lll1, this.loop2] //分割代入で見やすく。
-		str += `//${l1.toLocaleString()}*(${l2}*code)`
-		str += `= ${(l1 * l2).toLocaleString()} \n`
+		let [l1, l2] = [this.loop1, this.loop2] //分割代入で見やすく。
+		str += `//${l1.toLocaleString()}*(${l2}*fn)`
+		str += `= ${(l1 * l2).toLocaleString()}*fn \n`
 
-		this.times = this.times.map(t => t.toFixed(1))
-		let max = Math.max(...this.times) //型がNumに戻る
-		let maxlen = String(max.toFixed(1)).length
-		this.times = this.times.map(t => ('   ' + t).slice(-maxlen))
-		log(max, maxlen)
+		//一回配列にコピー
+		let time_arr = objArr.map(o => o.time)
+		//最大値
+		const max = Math.max(...time_arr)//objArr.map(o => o.time)) 
 
-		for (let i = 0, l = this.funcs.length; i < l; i++) {
-			let timeStr = this.times[i]
-			let graf = this.mgraf(this.times[i], max)
-			str += `/*${graf} ${timeStr}ms */ ${this.head}${this.funcs[i]})\n`
+		// objArr = objArr.map(o => o.time.toFixed(1))
+		// time_arr = objArr.map(o => o.time.toFixed(1))
+		//time_arr = time_arr.map(t => t.toFixed(1))
+
+		objArr.map(o => o.time_str = o.time.toFixed(1))
+		// let maxlen = max.toFixed(1).length
+		// let time_strArray = objArr.map(o => o.time_str)
+		// time_strArray = this._padding(time_strArray)
+		// objArr.map((o, i) => o.timeStr = time_strArray[i])
+
+		// let result_arr = objArr.map(o => o.result)
+		// result_arr = this._padding(result_arr)
+		// objArr.map((o, i) => o.result_pad = result_arr[i])
+
+		// let tmp_arr = this._padding(objArr.map(val => val.type))
+		// objArr.map((o, i) => o.type = tmp_arr[i])
+
+		this._padding2(objArr, 'time_str')
+		this._padding2(objArr, 'result', 'result_pad')
+		this._padding2(objArr, 'type')
+		this._padding2(objArr, 'func', undefined, 'left')
+
+		//mapでiで戻す。
+
+		for (let [key, val] of objArr.entries()) {
+			//配列をobjに追加。
+			//val.timeStr = time_arr[key]
+			val.graf = this.create_graf(val.time, max)
 		}
+		// log(1, JSON.stringify(objArr.entries(), null, '  '))
+		// log(JSON.stringify(objArr, null, '  '))
+
+		log(max)
+
+		// for (let i = 0, l = this.funcs.length; i < l; i++) {
+		// 	let timeStr = time_arr[i]
+		// 	let graf = this.mgraf(time_arr[i], max)
+		// 	str += `/*${graf} ${timeStr}ms */ ${this.head}${this.funcs[i]})\n`
+		// }
+		// for (let [key, val] of Object.entries(objArr)) {
+		// 	str += `/*${val.graf} ${val.timeStr}ms */ ${this.head}${val.func})\n`
+		// }
+		objArr.map(o =>
+			str += `${o.func} // ${o.graf} ${o.time_str}ms ${o.result_pad} ${o.type} |\n`
+			// str += `/* ${o.graf} ${o.timeStr}ms ${o.result_pad} ${o.type} */ ${this.head}${o.func})  \n`
+		)
 		log(str)
 	}
-	mgraf(val, max) {
-		let len = 10,
-			s = '#_'
+
+	create_graf(val, max) {
+		let len = 10, s = '#.'
 		let count = Math.round(val / max * len)
 		let str = s[0].repeat(count) + s[1].repeat(len - count)
 		return str
 	}
 }
 
-let b = new Bench(1000, 100)
+let b
+b = new Bench(1000, 100)
 let r,
-	str = 'aaa.bbb.ccc'
+	str = 'aa.bb.cc'
 //b.add(() => null);
-b.add(() => str.replace(/\./g, 'o'))
-b.add(() => str.replace(/\./g, 'o'))
-b.add(() => str.replace(new RegExp('\\.', 'g'), 'o'))
-b.add(() => str.split('.').join('o'))
-b.add(() => null)
-b.add(() => 0)
-b.add(() => undefined)
-b.add(() => str.replace(/\./g, 'o'))
-b.add(() => str.replace('.', 'o'))
+b.add(() => str.replace(/\./g, '-'))
+b.add(() => str.replace(/\./g, '-'))
+b.add(() => str.replace(new RegExp('\\.', 'g'), '-'))
+b.add(() => str.split('.').join('-'))
+b.add(() => str.replace(/\./g, '-'))
+b.add(() => str.replace('.', '-'))
 
-b.play()
+// b.print_result()
+
+b = new Bench(10, 100000)
+b.add(() => !1)
+b.add(() => !1)
+b.add(() => false)
+b.add(() => Boolean(0))
+b.add(() => {a: 100})
+b.add(() => [11, 22])
+b.add(() => 100)
+b.add(() => '日本語')
+b.add(() => '123')
+b.add(() => undefined)
+b.add(() => `${undefined}`)
+b.add(() => String(undefined))
+b.add(() => new String(111))
+b.add(() => String(111))
+b.add(() => ('' + undefined))
+b.add(() => (undefined + ''))
+// b.play()
 b.print_result()
 //log(str.replace(RegExp("\\.", "g"), "-"));
-console.log(123)
