aa1e299442e10b66d0a5ca9432354bf10ee9d1d5
esabox <esabox@live.jp>
2021-01-02 11:08:07 +0900

    Create local_file_reuire.user.js

---
+++ local_file_reuire.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         aalocal_load@codepen
+// @namespace    https://gist.github.com/esabox/
+// @updateURL    https://github.com/esabox/user.js/raw/master/local_file_reuire.user.js
+// @homepageURL  https://github.com/esabox/user.js/raw/master/local_file_reuire.user.js
+// @version 2019.12.02.171237
+// @author       nank
+// @match        *://*/*
+// @exclude      ^https://codepen.io/*
+// @exclude     https://docs.google.com/*
+// @grant	GM_registerMenuCommand
+// @grant 	GM_getValue
+// @grant 	GM_setValue
+// @grant 	GM_deleteValue
+// @grant 	GM_listValues
+// @grant   GM_xmlhttpRequest
+// @noframes
+// x@require       http://localhost:8888/lib/FileSaver.js
+// x@require       http://localhost:8888/lib/jszip.js
+// @require       file:///Users/kazoku/Desktop/code/mypo.user.js
+// x@require      http://localhost:8888/mypo.user.js
+// ==/UserScript==
+
+
+console.log(11)
